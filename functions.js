aeac583a5839a25a4d47d8b6235e4538c1c042b3
luchocarlos <luchopulimar@gmail.com>
2026-02-02 10:49:31 -0500

    factorizado

--- functions.js
+++ functions.js
@@ -1,34 +1,25 @@
-document.addEventListener('DOMContentLoaded', function() {
-  let coin = document.getElementById('coin');
+function flipCoin(){
+  let coin = document.getElementById("coin");
+  
+  coin.classList.remove("heads","tails");
 
-  coin.addEventListener('click', function() {
-    flipCoin();
+  void coin.offsetWidth;
 
-    let flipResult = Math.random();
-    coin.className = '';
-
-    setTimeout(function() {
-      if (flipResult <= 0.5) {
-        coin.classList.add('heads');
-        document.querySelector("#result").textContent = "Heads";
-
-        console.log('It is head');
+  let randomNumber = Math.floor(Math.random() * 2) + 1; 
+  
+  setTimeout(function(){
+     if(randomNumber == 1){
+       coin.classList.add('heads');
+       document.querySelector("#result").textContent = "Heads";
+       console.log('It is head'); 
       } else {
-        coin.classList.add('tails');
-        document.querySelector("#result").textContent = "Tails";
-        console.log('It is tails');
-      }
-    }, 100);
-  });
+         coin.classList.add('tails');
+         document.querySelector("#result").textContent = "Tails";
+         console.log('It is tails'); 
+             } 
+    } , 100);
 
-  function flipCoin() {
-    coin.classList.add('flip'); // Agrega una clase para la animación de giro
-    
-    
-    setTimeout(function() {
-      coin.classList.remove('flip');
-    }, 1000); // Elimina la clase después de la animación para que pueda repetirse
-  }
-});
+}
 
-      
+
+ 
